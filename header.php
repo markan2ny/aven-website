e15e2b55d9cb3f4ea5bc32ce0085e80ce6dcacfb
unknown <admin@MAC-IT.avendomain.local>
2022-01-13 08:13:58 +0800

    Added Barlow font

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php if( is_front_page(  ) ):?>
 		<div id="wrapper-navbar">
 	<?php else: ?>
-		<div id="wrapper-navbar">
+		<div id="wrapper-navbar-page">
 	<?php endif; ?>
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
